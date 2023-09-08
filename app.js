8846698b86858f98bcea80c8f8788cdd4ae9b4d1
Tiago Rodrigues <xtiago.rodrigues12@gmail.com>
2023-09-08 16:24:06 -0300

    Else e comentários

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 alert("Boas vindas ao jogo do número secreto");
 let numeroSecreto = 5;
+console.log(numeroSecreto)
 let chute = prompt("Escolha um número entre 1 e 10");
+// teste de comentáriossss //
 
+
+// Se o chute for igual ao número secreto //
 if (numeroSecreto == chute) {
     console.log("Isso aí! Você descobriu o número secreto. Brabíssimo (5)");
+} else {
+    alert("Você errou. Tente novamente!");
 }
+
