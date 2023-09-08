b3320ac2260b556c78c87d2e60a504f3ce7bb28f
Tiago Rodrigues <xtiago.rodrigues12@gmail.com>
2023-09-08 16:49:21 -0300

    Concatenação

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 alert("Boas vindas ao jogo do número secreto");
-let numeroSecreto = 5;
+let numeroSecreto = 9;
 console.log(numeroSecreto)
 let chute = prompt("Escolha um número entre 1 e 10");
 // teste de comentáriossss //
 
 
 // Se o chute for igual ao número secreto //
-if (numeroSecreto == chute) {
-    console.log("Isso aí! Você descobriu o número secreto. Brabíssimo (5)");
+if (chute == numeroSecreto) {
+    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}` );
 } else {
     alert("Você errou. Tente novamente!");
 }
